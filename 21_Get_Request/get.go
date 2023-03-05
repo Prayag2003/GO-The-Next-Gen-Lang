57219d16236b2fd1d0ba5d72cdf9494788e39d75
Prayag <prayagbhatt2003@gmail.com>
2023-03-05 23:15:26 +0530

    Get And Post Requests

--- 21_Get_Request/get.go
+++ 21_Get_Request/get.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
 
-	url := "http://localhost:8000"
+	url := "http://localhost:3000/get"
 	fmt.Println("GET Request in Go ... ")
 	PerformGetRequest(&url)
 
@@ -15,13 +17,35 @@ func main() {
 
 func PerformGetRequest(myURL *string) {
 
+	// Making a Web request
 	response, err := http.Get(*myURL)
 	if err != nil {
 		panic(err)
 	}
 
-	defer response.Body.Close()
-	fmt.Println("Generated Response is ", response.StatusCode)
+	defer response.Body.Close() // it's user's responsibility to close the response once the task is done
+
+	fmt.Println("Status Code is ", response.StatusCode)
 	fmt.Println("Content Length is ", response.ContentLength)
+
+	// reading all the content from the body of the Response
+	content, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	// wrapping the content into a string so as to avoid displaying of the bytes
+	fmt.Println(string(content))
+
+	/* Another Method to Write the Content into String using Strings.builder */
+
+	// https://pkg.go.dev/strings#Builder
+
+	var responseString strings.Builder  
+	bytecount, _ := responseString.Write(content)
+	fmt.Println("No of Bytes in the String is ", bytecount)
+
+	printContent := responseString.String()
+	fmt.Println("Content is ", printContent)
 
 }
